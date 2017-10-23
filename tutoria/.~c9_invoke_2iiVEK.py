59c89de1fbd8dc428fa376e46905fc06ca56ad18
Abhinav Goyal <abhinav.goyal@live.com>
2017-10-23 21:29:20 +0000

    fix settings

--- tutoria/.~c9_invoke_2iiVEK.py
+++ tutoria/.~c9_invoke_2iiVEK.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'student_app',
+    'review_app',
     'tutor_app',
     'review_app',
 ]

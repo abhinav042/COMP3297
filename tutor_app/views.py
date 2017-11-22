ea8d1a4cbd82c66e49af6b98d5c70ee7da6a9d56
Abhinav Goyal <abhinav.goyal@live.com>
2017-11-22 15:12:11 +0000

    add block_session

--- tutor_app/views.py
+++ tutor_app/views.py
@@ -7,11 +7,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from tutor_app.models import Tutor
 from tutor_app.forms import EditProfileForm, EditUserForm
-from datetime import datetime,date 
-from datetime import timedelta
+import datetime
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
+from session_app.models import Session
 
 # Create your views here.
 def index(request):
@@ -116,15 +116,39 @@ def datetime_range(start, end, delta):
         current += delta
             
 def timeSlots(request):
-    start = datetime.today()
-    end = start + timedelta(1)
-    print(end)
-    dts = [dt.strftime('%Y-%m-%d T%H:%M Z') for dt in 
-       datetime_range(start, datetime(2017, 11, 22, 9+10),timedelta(minutes=30))]
-    return render(request,'tutor_app/timeSlots.html',{'timeSlot':dts})
+    preset_time = []
+    time_slot = []
+    elem_time = datetime.time(9, 00)
+    time_delta = datetime.timedelta(minutes = 30)
+    for i in range(18):
+        temp_time = datetime.datetime.combine(datetime.date(1, 1, 1), elem_time)
+        preset_time.append(elem_time)
+        elem_time = (temp_time + time_delta).time()
+    
+    curr_date = datetime.datetime.now().date()
+    date_delta = datetime.timedelta(days = 1)
+    for i in range(14):
+        for j in range(18):
+            time_to_append = datetime.datetime.combine(curr_date, preset_time[j]).isoformat()
+            time_slot.append(time_to_append)
+        temp_date = datetime.datetime.combine(curr_date, datetime.time(1,1))
+        curr_date = (temp_date + date_delta).date()
+    
+    # end = start + timedelta(1)
+    # print(end)
+    # dts = [dt.strftime('%Y-%m-%d %H:%M ') for dt in 
+    #   datetime_range(start, datetime(2017, 11, 22, 9+10),timedelta(minutes=30))]
+       
+    ## returning timeslot array to the template
+    return render(request,'tutor_app/timeSlots.html',{'timeSlot':time_slot})
 
 def blockSession(request):
     timeSlot = request.GET.get("time")
+    user_id=request.user
+    tutor = Tutor.objects.get(user=user_id)
+    session = Session(tutor = tutor, session_time = timeSlot, status = 1)
+    session.save()
+    
     
     
 def change_password(request):
